0479eadedfb55cf1bb8b00e463f9a9b7e040289a
Jun <geffrey90@hotmail.com>
2019-03-17 00:17:20 +0800

    added basic main file

---
+++ main.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+	//Go use Static types for variable so need to declare type of variable
+	//and only contain that type of variable in
+	//var card string = "Ace of Spades" (this is longer form)
+	card := "Ace of Spades"
+	//make the compiler to figure out what type is variable by value of it
+	// := used only for new variable not reassign value of variable
+	//var: short for variable that card is variable
+	//card: name of variable
+	//string: type of variable
+	//"Acce of Spaces": value contain in variable
+	fmt.Println(card)
+}
