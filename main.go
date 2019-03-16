370b0abf62ace1b5f8221b2846524493b4b3e89e
Jun <geffrey90@hotmail.com>
2019-03-17 00:26:53 +0800

    added new function call newCard

--- main.go
+++ main.go
@@ -6,7 +6,7 @@ func main() {
 	//Go use Static types for variable so need to declare type of variable
 	//and only contain that type of variable in
 	//var card string = "Ace of Spades" (this is longer form)
-	card := "Ace of Spades"
+	card := newCard()
 	//make the compiler to figure out what type is variable by value of it
 	// := used only for new variable not reassign value of variable
 	//var: short for variable that card is variable
@@ -14,4 +14,8 @@ func main() {
 	//string: type of variable
 	//"Acce of Spaces": value contain in variable
 	fmt.Println(card)
+}
+
+func newCard() string { //after the () with space define return data type
+	return "five of diamonds"
 }
