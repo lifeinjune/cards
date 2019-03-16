40e152e7e88e308abeff208001a45b129a0730e7
Jun <geffrey90@hotmail.com>
2019-03-17 00:49:19 +0800

    added slice for cards and print each element in cards slice

--- main.go
+++ main.go
@@ -6,16 +6,29 @@ func main() {
 	//Go use Static types for variable so need to declare type of variable
 	//and only contain that type of variable in
 	//var card string = "Ace of Spades" (this is longer form)
-	card := newCard()
+	cards := []string{"Ace of Diamonds", newCard()} //slice declaration
+	cards = append(cards, "Six of spades")          //add element in slice at the back of the slice
+	//append returns new slice not modify slice
 	//make the compiler to figure out what type is variable by value of it
 	// := used only for new variable not reassign value of variable
 	//var: short for variable that card is variable
 	//card: name of variable
 	//string: type of variable
 	//"Acce of Spaces": value contain in variable
-	fmt.Println(card)
+	for i, card := range cards {
+		/*
+			i: index of the slice
+			card: value of the element in the slice
+			range cards: loop through each element in data structure
+		*/
+		fmt.Println(i, card)
+	}
+
 }
 
 func newCard() string { //after the () with space define return data type
 	return "five of diamonds"
 }
+
+//functions can be called in different file but same package without
+//import statement
