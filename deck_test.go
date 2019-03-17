706fc7a295fd16da92c421b020153c512da42d2a
Jun <geffrey90@hotmail.com>
2019-03-18 03:40:10 +0800

    fixed deck_test.go

--- deck_test.go
+++ deck_test.go
@@ -25,13 +25,13 @@ func TestNewDeck(t *testing.T) { //test function start with uppercase
 }
 
 func TestSaveToDeckAndNewDeckFromFile(t *testing.T) {
-	os.Remove("_decktesting")
-	deck := newDeck()
-	deck.saveToFile("_decktesting")
-	loadDeck := newDeckFromFile("_decktesting")
-	if len(loadDeck) != 52 { // if test fail
+	os.Remove("_decktesting")                   //use os.Remove function to remover the test file if there is one
+	deck := newDeck()                           //create deck
+	deck.saveToFile("_decktesting")             //save the deck to file
+	loadDeck := newDeckFromFile("_decktesting") //load the deck from file
+	if len(loadDeck) != 52 {                    // if test fail
 		t.Errorf("Expected deck length of 52 , but got %v", len(loadDeck))
 		//tell test handler test is fail
 	}
-	os.Remove("_decktesting")
+	os.Remove("_decktesting") //remove the test file
 }
